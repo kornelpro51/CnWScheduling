348225f52619eb3fa7c6b57691172fc47e914557
Kornel Dembek <kornelpro51@gmail.com>
2014-05-23 11:39:44 +0200

    create ui calendar, create nodemon.json

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 // Declare app level module which depends on filters, and services
-angular.module('scheduler', ['ngResource', 'ngRoute', 'ui.bootstrap', 'ui.date'])
+angular.module('scheduler', ['ngResource', 'ngRoute', 'ui.bootstrap', 'ui.date', 'ui.calendar'])
   .config(['$routeProvider', function ($routeProvider) {
     $routeProvider
       .when('/', {
--- public/js/home/home-controller.js
+++ public/js/home/home-controller.js
@@ -1,3 +1,66 @@
 angular.module('scheduler')
-  .controller('HomeController', ['$scope', function ($scope) {
-  }]);
+	.controller('HomeController', ['$scope', '$location','$modal', function ($scope, $location, $modal) {
+		$scope.uiConfig = {
+			calendar: {
+				height: 450,
+				editable: true,
+				selectable: true,
+				header: {
+					left: 'month agendaWeek agendaDay',
+					center: 'title',
+					right: 'today prev,next'
+				},
+				eventClick: function(calEvent, jsEvent, view) {
+					$scope.alertEventClick(calEvent, jsEvent, view, this);
+				},
+				dayClick: function(date, allDay, jsEvent, view) {
+					$scope.alertDayClick(date, allDay, jsEvent, view, this);
+				},
+			}
+		}
+		$scope.scheduleSource = [];
+		$scope.alertEventClick = function (calEvent, jsEvent, view, target) {
+			console.log(calEvent, jsEvent, view);
+		}
+		$scope.alertDayClick = function (date, allDay, jsEvent, view, target) {
+			console.log(date, allDay, jsEvent, view, target);
+			$scope.openWizard();
+		}
+
+		$scope.items = ['item1', 'item2', 'item3'];
+
+		$scope.openWizard = function(size) {
+			var modalInstance = $modal.open({
+		      templateUrl: 'myModalContent.html',
+		      controller: 'ModalInstanceCtrl',
+		      size: size,
+		      resolve: {
+		        items: function () {
+		          return $scope.items;
+		        }
+		      }
+		    });
+
+		    modalInstance.result.then(function (selectedItem) {
+		      $scope.selected = selectedItem;
+		    }, function () {
+		      $log.info('Modal dismissed at: ' + new Date());
+		    });
+		}
+	}]);
+
+angular.module('scheduler')
+	.controller('ModalInstanceCtrl', ['$scope', '$modalInstance', 'items', function ($scope, $modalInstance, items) {
+  $scope.items = items;
+  $scope.selected = {
+    item: $scope.items[0]
+  };
+
+  $scope.ok = function () {
+    $modalInstance.close($scope.selected.item);
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
+}]);
