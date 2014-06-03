aedb1a60975e1571d6ad5e8a3a14c655e38d6c52
Kornel Dembek <kornelpro51@gmail.com>
2014-06-03 22:35:59 +0200

    fix issue #7 no view option

--- public/js/home/home-controller.js
+++ public/js/home/home-controller.js
@@ -55,12 +55,12 @@ angular.module('scheduler')
                 angular.forEach(data, function(value, key) {
                     value.date = dateFormat(value.starts_at, "mm/dd/yyyy");
                     value.startTime = dateFormat(value.starts_at, "hh:MM TT");
-                    value.endTime = dateFormat(value.starts_at, "hh:MM TT");
+                    value.endTime = dateFormat(value.ends_at, "hh:MM TT");
                 })
             } else {
                 data.date = dateFormat(data.starts_at, "mm/dd/yyyy");
                 data.startTime = dateFormat(data.starts_at, "hh:MM TT");
-                data.endTime = dateFormat(data.starts_at, "hh:MM TT");
+                data.endTime = dateFormat(data.ends_at, "hh:MM TT");
             }
         }
 
@@ -82,47 +82,68 @@ angular.module('scheduler')
         }
         
         $scope.openWizard = function(date, initialData) {
-            var modalInstance = $modal.open({
-              templateUrl: 'myModalContent.html',
-              controller: 'ModalInstanceCtrl',
-              resolve: {
-                paramUsers: function () { 
-                    var pr = AppointmentService.getUserList();
-                    return pr;
-                },
-                paramTypes: function () {
-                    var pr = AppointmentService.getApptTypeList();
-                    return pr; 
-                },
-                pickupDate: function() { return date; },
-                initialData: function () { return initialData; }
-              }
-            });
+            if (typeof initialData == 'undefined') {
+                var modalInstance = $modal.open({
+                  templateUrl: 'createAppointmentModal.html',
+                  controller: 'CreateAppointmentCtrl',
+                  size: 'lg',
+                  resolve: {
+                    paramUsers: function () { 
+                        var pr = AppointmentService.getUserList();
+                        return pr;
+                    },
+                    paramTypes: function () {
+                        var pr = AppointmentService.getApptTypeList();
+                        return pr; 
+                    },
+                    pickupDate: function() { return date; },
+                    initialData: function () { return initialData; }
+                  }
+                });
 
-            modalInstance.result.then(function (appt) {
-                convertToDBFormat(appt.appointmentEvents);
-                if (typeof appt.appt_group_id == "undefined") { // new appointment is created.
-                    AppointmentService.createAppointment(appt).then(function(result) {
-                        if ( result.data && result.data.success) {
-                            appt.appt_group_id = result.data.result.appt_group_id;
-                        }
-                        $scope.appointmentInfos.push(appt);
-                        createFullcalendarEvent(appt);
-                    });
-                } else {
-                    //AppointmentService.updateAppointment(appt).then(function(result) {
-                    //    $scope.appointmentInfos.push(appt);
-                    //    createFullcalendarEvent(appt);
-                    //});
-                }
-            }, function () {
-                console.log('Modal dismissed at: ' + new Date());
-            });
+                modalInstance.result.then(function (appt) {
+                    convertToDBFormat(appt.appointmentEvents);
+                    if (typeof appt.appt_group_id == "undefined") { // new appointment is created.
+                        AppointmentService.createAppointment(appt).then(function(result) {
+                            if ( result.data && result.data.success) {
+                                appt.appt_group_id = result.data.result.appt_group_id;
+                            }
+                            $scope.appointmentInfos.push(appt);
+                            createFullcalendarEvent(appt);
+                        });
+                    } else {
+                        //AppointmentService.updateAppointment(appt).then(function(result) {
+                        //    $scope.appointmentInfos.push(appt);
+                        //    createFullcalendarEvent(appt);
+                        //});
+                    }
+                }, function () {
+                    console.log('Modal dismissed at: ' + new Date());
+                });
+            } else {
+                var modalInstance = $modal.open({
+                    templateUrl: 'viewAppointmentModal.html',
+                    controller: 'ViewAppointmentCtrl',
+                    size: 'lg',
+                    resolve: {
+                        paramUsers: function () { 
+                            var pr = AppointmentService.getUserList();
+                            return pr;
+                        },
+                        paramTypes: function () {
+                            var pr = AppointmentService.getApptTypeList();
+                            return pr; 
+                        },
+                        pickupDate: function() { return date; },
+                        initialData: function () { return initialData; }
+                    }
+                });
+            }
         }
     }]);
 
 angular.module('scheduler')
-    .controller('ModalInstanceCtrl', ['$scope', '$modalInstance', 'paramUsers', 'paramTypes', 'pickupDate', 'initialData', function ($scope, $modalInstance, paramUsers, paramTypes, pickupDate, initialData) {
+    .controller('CreateAppointmentCtrl', ['$scope', '$modalInstance', 'paramUsers', 'paramTypes', 'pickupDate', 'initialData', function ($scope, $modalInstance, paramUsers, paramTypes, pickupDate, initialData) {
     $scope.step = 0;    
     $scope.pickupDate = pickupDate;
     
@@ -145,6 +166,7 @@ angular.module('scheduler')
         changeMonth: true,
         yearRange: '1900:-0'
     };
+    $scope.dlgTitle = '';
 
 
 
@@ -257,6 +279,9 @@ angular.module('scheduler')
                 attendee.email = user.email;
                 attendee.editMode = false;
             });
+            $scope.dlgTitle = "Edit your appointment.";
+        } else {
+            $scope.dlgTitle = "Create your appointment.";
         }
     }
 
@@ -404,5 +429,78 @@ angular.module('scheduler')
     $scope.tooltip = {
         "title": "Hello Tooltip<br />This is a multiline message!",
         "checked": false
+    };
+}]);
+
+angular.module('scheduler')
+    .controller('ViewAppointmentCtrl', ['$scope', '$modalInstance', 'paramUsers', 'paramTypes', 'pickupDate', 'initialData', function ($scope, $modalInstance, paramUsers, paramTypes, pickupDate, initialData) {
+    
+    $scope.currentAppointment = null;
+    $scope.currentPos = 0;
+    $scope.endPos = 0;
+
+    $scope.data = {
+        attendees: [],
+        appointments: []
+    }
+
+    $scope.info = {
+        users: [],
+        types: []
+    }
+
+    function LoadInformations () {
+        if ( paramUsers.status == 200 && paramUsers.data.success ) {
+            $scope.info.users = jQuery.extend($scope.info.users, paramUsers.data.result);
+        }
+        if ( paramTypes.status == 200 && paramTypes.data.success ) {
+            $scope.info.types = jQuery.extend($scope.info.types, paramTypes.data.result);
+        }
+    }
+
+    function getUserInfo (userId) {
+        var userInfo = null;
+        angular.forEach($scope.info.users, function(user, idx) {
+            if(user.user_id == userId) {
+                userInfo = user;
+            }
+        });
+        return userInfo;   
+    }
+
+    function InitializeData() {
+        if ( initialData ) {
+            $scope.data = jQuery.extend({}, initialData);
+            if ($scope.data.appointmentEvents && $scope.data.appointmentEvents.length > 0) {
+                $scope.currentAppointment = $scope.data.appointmentEvents[0]
+                $scope.currentPos = 0;
+                $scope.endPos = $scope.data.appointmentEvents.length-1;
+            }
+            angular.forEach($scope.data.attendees, function(attendee, key) {
+                var user = getUserInfo(attendee.user_id);
+                attendee.firstName = user.given_name;
+                attendee.lastName = user.family_name;
+                attendee.email = user.email;
+            });
+        }
+    }
+
+    LoadInformations();
+    InitializeData();
+
+    $scope.nextAppointment = function() {
+        if ( $scope.currentPos < $scope.endPos ) {
+            $scope.currentPos = $scope.currentPos + 1;
+            $scope.currentAppointment = $scope.data.appointmentEvents[$scope.currentPos];
+        }
+    }
+    $scope.prevAppointment = function() {
+        if ( $scope.currentPos > 0 ) {
+            $scope.currentPos = $scope.currentPos - 1;
+            $scope.currentAppointment = $scope.data.appointmentEvents[$scope.currentPos];
+        }
+    }
+    $scope.close = function () {
+        $modalInstance.dismiss('cancel');
     };
 }]);
